33a91121eda6775ad713f1c35d902451b5d4e58c
hhazle <henryhazlegames@gmail.com>
2023-12-16 01:11:22 +0000

    curlpp time

--- WikiStocks/WikiStocks/Source.cpp
+++ WikiStocks/WikiStocks/Source.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-#include <D:Software\vcpkg\packages\curl_x64-windows\include\curl\curl.h>
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/easy.hpp>
+#include <curlpp/options.hpp>
+using namespace curlpp::options;
 
 int main()
 {
-	CURL* curl;
-	CURLcode res;
+	curlpp::Cleanup myCleanup;
 
-	curl = curl_easy_init();
+	std::cout << Url("https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/all-access/all-agents/Undertale/daily/20231214/20231214");
 }
 

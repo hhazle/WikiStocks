eee8de1224c72e49ab88f82e16a752473a193a7d
hhazle <henryhazlegames@gmail.com>
2023-12-16 01:38:56 +0000

    Added basic viewcount checker
    using curlpp you can input a article name and it will output a viewpoint. no intput validation or error checking yet.

--- WikiStocks/WikiStocks/Source.cpp
+++ WikiStocks/WikiStocks/Source.cpp
@@ -2,12 +2,37 @@
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/easy.hpp>
 #include <curlpp/options.hpp>
+#include <string>
+#include <sstream>
 using namespace curlpp::options;
 
 int main()
 {
-	curlpp::Cleanup myCleanup;
+	char loop = 'l';
+	while (loop == 'l')
+	{
+		curlpp::Cleanup myCleanup;
+		std::stringstream json;
 
-	std::cout << Url("https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/all-access/all-agents/Undertale/daily/20231214/20231214");
+		std::string urlString = "";
+		std::string article = "";
+		std::cout << "\nArticle: ";
+		std::cin >> article;
+
+		urlString = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/all-access/all-agents/" + article + "/daily/20231214/20231214";
+
+		json << Url(urlString);
+
+		std::string line;
+		while (!json.eof())
+		{
+			std::getline(json, line, ',');
+		}
+		std::cout << line << std::endl;
+		std::cout << line.substr(8, line.length() - 11) << std::endl;
+
+		std::cout << "Loop: ";
+		std::cin >> loop;
+	}
 }
 
